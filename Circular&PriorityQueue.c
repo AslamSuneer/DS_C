0f6119641b642f89734d82d4ff884c2eeedfdaf4
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2023-11-12 20:51:23 +0530

    Update Circular&PriorityQueue.c

--- Circular&PriorityQueue.c
+++ Circular&PriorityQueue.c
@@ -1,138 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_SIZE 5
-
-struct PriorityQueueNode {
-    int data;
-    int priority;
-};
-
-void circularQueueMenu();
-void priorityQueueMenu();
-void insertCircularQueue(int queue[], int *front, int *rear, int element);
-int deleteCircularQueue(int queue[], int *front, int *rear);
-void displayCircularQueue(int queue[], int front, int rear);
-void insertPriorityQueue(struct PriorityQueueNode queue[], int *rear, int element, int priority);
-struct PriorityQueueNode deletePriorityQueue(struct PriorityQueueNode queue[], int *rear);
-void displayPriorityQueue(struct PriorityQueueNode queue[], int rear);
-
-int main() {
-    int circularQueue[MAX_SIZE], circularFront = -1, circularRear = -1;
-    struct PriorityQueueNode priorityQueue[MAX_SIZE];
-    int priorityRear = -1, choice;
-
-    do {
-        printf("\nMain Menu\n1. Circular Queue\n2. Priority Queue\n3. Exit\nEnter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1: circularQueueMenu(&circularFront, &circularRear); break;
-            case 2: priorityQueueMenu(&priorityRear); break;
-            case 3: printf("Exiting the program.\n"); break;
-            default: printf("Invalid choice. Please try again.\n");
-        }
-    } while (choice != 3);
-
-    return 0;
+#include<stdio.h>
+int A[20],size,front,rear;
+void ENQUEUE_CQ(int item){
+if((rear+1)%size==front)
+printf("Queue is FULL\n");
+else if(rear==-1)
+{
+front=0;
+rear=0;
+A[rear]=item;
 }
-
-void circularQueueMenu(int *front, int *rear) {
-    int circularQueue[MAX_SIZE], choice, element;
-
-    do {
-        printf("\nCircular Queue Menu\n1. Insert\n2. Delete\n3. Display\n4. Back to Main Menu\nEnter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1: printf("Enter the element to insert: "); scanf("%d", &element);
-                    insertCircularQueue(circularQueue, front, rear, element); break;
-            case 2: element = deleteCircularQueue(circularQueue, front, rear);
-                    if (element != -1) printf("Deleted element: %d\n", element); break;
-            case 3: displayCircularQueue(circularQueue, *front, *rear); break;
-            case 4: printf("Returning to the main menu.\n"); break;
-            default: printf("Invalid choice. Please try again.\n");
-        }
-    } while (choice != 4);
+else
+{
+rear=(rear+1)%size;
+A[rear]=item;
 }
-
-void insertCircularQueue(int queue[], int *front, int *rear, int element) {
-    if ((*rear + 1) % MAX_SIZE == *front) { printf("Queue is full. Cannot insert.\n"); return; }
-    if (*front == -1) *front = *rear = 0;
-    else *rear = (*rear + 1) % MAX_SIZE;
-    queue[*rear] = element;
-    printf("Element %d inserted successfully.\n", element);
 }
-
-int deleteCircularQueue(int queue[], int *front, int *rear) {
-    int element;
-    if (*front == -1) { printf("Queue is empty. Cannot delete.\n"); return -1; }
-    element = queue[*front];
-    if (*front == *rear) *front = *rear = -1;
-    else *front = (*front + 1) % MAX_SIZE;
-    return element;
+void DEQUEUE_CQ()
+{
+if(front==-1)
+printf("Queue is EMPTY");
+else if(front==rear)
+{
+printf("Deleted item is %d", A[front]); 
+front=-1; rear=-1;
 }
-
-void displayCircularQueue(int queue[], int front, int rear) {
-    if (front == -1) { printf("Queue is empty.\n"); return; }
-    printf("Circular Queue: ");
-    int i = front;
-    do { printf("%d ", queue[i]); i = (i + 1) % MAX_SIZE; } while (i != (rear + 1) % MAX_SIZE);
-    printf("\n");
+else
+{
+printf("Deleted item is %d",A[front]); 
+    front=(front+1)%size;
 }
-
-void priorityQueueMenu(int *rear) {
-    struct PriorityQueueNode priorityQueue[MAX_SIZE];
-    int choice, element, priority;
-
-    do {
-        printf("\nPriority Queue Menu\n1. Insert\n2. Delete\n3. Display\n4. Back to Main Menu\nEnter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1: printf("Enter the element to insert: "); scanf("%d", &element);
-                    printf("Enter the priority of the element: "); scanf("%d", &priority);
-                    insertPriorityQueue(priorityQueue, rear, element, priority); break;
-            case 2: struct PriorityQueueNode deletedNode = deletePriorityQueue(priorityQueue, rear);
-                    if (deletedNode.data != -1) printf("Deleted element: %d with priority %d\n", deletedNode.data, deletedNode.priority); break;
-            case 3: displayPriorityQueue(priorityQueue, *rear); break;
-            case 4: printf("Returning to the main menu.\n"); break;
-            default: printf("Invalid choice. Please try again.\n");
-        }
-
-    } while (choice != 4);
 }
-
-void insertPriorityQueue(struct PriorityQueueNode queue[], int *rear, int element, int priority) {
-    if (*rear == MAX_SIZE - 1) { printf("Queue is full. Cannot insert.\n"); return; }
-    *rear = *rear + 1;
-    queue[*rear].data = element;
-    queue[*rear].priority = priority;
-    printf("Element %d with priority %d inserted successfully.\n", element, priority);
+void DISPLAY_CQ()
+{ 
+    int i;
+if(front==-1)
+printf("Queue is EMPTY");
+else if(front<=rear)
+{ 
+    for(i=front;i<=rear;i++) 
+    printf("%d\t", A[i]);
 }
-
-struct PriorityQueueNode deletePriorityQueue(struct PriorityQueueNode queue[], int *rear) {
-    struct PriorityQueueNode deletedNode = { -1, -1 };
-    if (*rear == -1) { printf("Queue is empty. Cannot delete.\n"); return deletedNode; }
-    int highestPriorityIndex = 0;
-
-    for (int i = 1; i <= *rear; i++)
-        if (queue[i].priority < queue[highestPriorityIndex].priority)
-            highestPriorityIndex = i;
-
-    deletedNode = queue[highestPriorityIndex];
-
-    for (int i = highestPriorityIndex; i < *rear; i++)
-        queue[i] = queue[i + 1];
-
-    *rear = *rear - 1;
-
-    return deletedNode;
+else
+{
+    for(i=front;i<=(size-1);i++) printf("%d\t",A[i]); 
+    for(i=0;i<=rear;i++)
+    printf("%d\t", A[i]);
 }
-
-void displayPriorityQueue(struct PriorityQueueNode queue[], int rear) {
-    if (rear == -1) { printf("Queue is empty.\n"); return; }
-    printf("Priority Queue: ");
-    for (int i = 0; i <= rear; i++) printf("(%d, %d) ", queue[i].data, queue[i].priority);
-    printf("\n");
+printf("\n");
+} 
+void main()
+{
+int item,opt;
+front=-1;
+rear=-1;
+printf("Enter the size of the Queue: "); 
+scanf("%d", &size);
+do
+{
+printf("1.ENQUEUE\n2.DEQUEUE\n3.DISPLAY\n4.EXIT\n"); 
+printf("\nEnter the option:");
+scanf("%d",&opt);
+switch(opt)
+{
+ case 1: printf("Enter element to insert: "); 
+scanf("%d",&item);
+ENQUEUE_CQ(item); break;
+case 2: DEQUEUE_CQ(); break;
+case 3: DISPLAY_CQ();break;
+case 4: break;
+default: printf("Invalid option"); 
+}
+}while(opt!=4);
 }
